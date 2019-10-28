a79d2d5cf8031c30caceb41842e21e6e5615d4e7
Cyprian Bowen <branco1lelgo@gmail.com>
2019-10-28 22:25:50 +0300

    update changes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quotes } from '../quotes'
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+  quotes:Quotes[] = [
+    new Quotes('Watch finding Nemo', 'Find an online version and watch merlin find his son', 'John'),
+    new Quotes('Buy Cookies','I have to buy cookies for the parrot','Michael'),
+    new Quotes('Get new Phone Case','Diana has her birthday coming up soon', 'Mathew'),
+    new Quotes('Get Dog Food','Pupper likes expensive snacks', 'Mathew'),
+  ]
 
   constructor() { }
 
