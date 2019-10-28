a23c6c66dbb916da6cf4f86a25250869b12205e8
Cyprian Bowen <branco1lelgo@gmail.com>
2019-10-28 22:25:15 +0300

    add emitter events

--- src/app/form-detail/form-detail.component.ts
+++ src/app/form-detail/form-detail.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Quotes } from '../quotes'
+import { FormGroup, FormBuilder, Validators } from '@angular/forms'
+import { Quote } from '@angular/compiler/src/expression_parser/ast';
 @Component({
   selector: 'app-form-detail',
   templateUrl: './form-detail.component.html',
@@ -7,6 +9,16 @@ import { Quotes } from '../quotes'
 })
 export class FormDetailComponent implements OnInit {
 
+@Output() postQuote = new EventEmitter()
+quotePublisher:string
+quoteTxt:string
+quoteAuthor:string
+createdQuote:any
+
+submitQuote(){
+  this.createdQuote = new Quotes(this.quotePublisher,this.quoteTxt, this.quoteAuthor)
+  
+}
   constructor() { }
 
   ngOnInit() {
