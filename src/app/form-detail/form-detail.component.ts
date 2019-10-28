46f45e2a44edc04b9f795d94ed91a3e18f55c208
Cyprian Bowen <branco1lelgo@gmail.com>
2019-10-28 20:12:53 +0300

    add imported class

--- src/app/form-detail/form-detail.component.ts
+++ src/app/form-detail/form-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quotes } from '../quotes'
 @Component({
   selector: 'app-form-detail',
   templateUrl: './form-detail.component.html',
